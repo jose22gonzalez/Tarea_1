753763a95b480ed37e3f427498f1cd9e62994cee
jose22gonzalez <97250949+jose22gonzalez@users.noreply.github.com>
2022-01-22 14:44:29 -0400

    Jose Alberto Gonzalez
    Tarea 1, Ejercicios Capitulo 1,2,3.

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,9 @@
+﻿//Ejercicio 1
+Console.WriteLine("Jose Alberto Gopnzalez");
+Console.ReadLine();
+
+
+//Ejercicio 5
+Console.WriteLine("Hello, My name is Jose Alberto");
+Console.WriteLine("I am a college student at Universidad Catolica Nordestana");
+Console.Write("...");
